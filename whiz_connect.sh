45d544b659af1e77aabfdcc75603e77dd4f66f55
Minsoo Kim <minsooerickim@gmail.com>
2023-03-25 15:13:56 -0700

    :sparkles: add user menu to choose encryption options

--- whiz_connect.sh
+++ whiz_connect.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-id_ed_exists = false
+key_type=""
+echo
 
 # check if rsa/id_ed22519 key already exists
 if [ -e ~/.ssh/id_rsa.pub ] || [ -e ~/.ssh/id_ed25519.pub ]; then
     # id_ed25519 is safer so priority
     if [ -e ~/.ssh/id_ed25519.pub ]; then
-        id_ed_exists = true
+        key_type="ed25519"
         echo "detected existing id_ed22519.pub"
     else
+        key_type="rsa"
         echo "detected existing id_rsa.pub"
     fi
 else
-    echo "generating new id_rsa"
+    echo "Choose the type of SSH key to generate:"
+    echo "1) id_rsa (RSA)"
+    echo "2) id_ed25519 (EdDSA)"
+    echo
+    echo -n "Enter your choice (1 or 2): "
+    read choice
 
-    # saves to /.ssh/
-    # no passphrase comment this line out if you want a passphrase
-    # passphrase confirmation
-    # echo -en "\n\n\n" | ssh-keygen -t rsa
-    echo | ssh-keygen -P '' -t rsa
+    if [ "$choice" -eq 1 ]; then
+        key_type="rsa"
+        echo "Generating id_rsa key..."
+    elif [ "$choice" -eq 2 ]; then
+        key_type="ed25519"
+        echo "Generating id_ed25519 key..."
+    else
+        echo "Invalid choice. Exiting."
+        exit 1
+    fi
 
-    # id_rsa & id_rsa.pub generated
-    echo "id_rsa & id_rsa.pub successfully generated in /.ssh/"
+    ssh-keygen -t "$key_type"
+    echo "SSH key successfully generated."
+
+    echo "$key_type successfully generated!"
 fi
 
 # get user and remote host
@@ -30,7 +44,7 @@ echo "type in the ssh command to your remote server (ex. ssh mkim410@bolt.cs.ucr
 read ssh_remote_host
 
 # move public key to remote server
-if [ "$id_ed_exists" = true ]; then
+if [ "$key_type" = "ed25519" ]; then
     ssh-copy-id -i ~/.ssh/id_ed25519.pub $ssh_remote_host
 else
     ssh-copy-id -i ~/.ssh/id_rsa.pub $ssh_remote_host
