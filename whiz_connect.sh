e73e9a2730119ee246e6b8365ac5cc477c1b2e2f
Minsoo Kim <minsooerickim@gmail.com>
2023-04-27 14:08:20 -0700

    :lipstick: remove color codes

--- whiz_connect.sh
+++ whiz_connect.sh
@@ -14,9 +14,9 @@ if [ -e ~/.ssh/id_rsa.pub ] || [ -e ~/.ssh/id_ed25519.pub ]; then
         echo "detected existing id_rsa.pub"
     fi
 else
-    echo -e "\e[1mChoose the type of SSH key to generate:\e[0m"
-    echo -e "1) \e[32mid_rsa (RSA)\e[0m"
-    echo -e "2) \e[32mid_ed25519 (EdDSA)\e[0m"
+    echo -e "Choose the type of SSH key to generate:"
+    echo -e "1) id_rsa (RSA)"
+    echo -e "2) id_ed25519 (EdDSA)"
     echo
     echo -n "Enter your choice (1 or 2): "
     read choice
@@ -34,7 +34,7 @@ else
 
     ssh-keygen -t "$key_type"
 
-    echo -e "\e[32m$key_type successfully generated!\e[0m"
+    echo -e "$key_type successfully generated!"
 fi
 
 # get user and remote host
@@ -50,6 +50,6 @@ else
 fi
 
 # done
-echo -e "\e[32mwhiz whiz!!\e[0m"
+echo -e "whiz whiz!!"
 
 exit
